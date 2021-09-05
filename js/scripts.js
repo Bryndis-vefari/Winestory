fc28f2084042550a0545f22cf9af810871414b94
Heba Farestveit Úlfarsdóttir <heba.farestveit@gmail.com>
2021-09-05 00:03:29 +0000

    Add lazyloading for images. Fix footer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+// Lazyloading for images
+var lazyLoadInstance = new LazyLoad({
+    // Your custom settings go here
+
+});
+
+
+//Mobile menu dropdown
 document.getElementById("hamburger").addEventListener("click", function(){
     this.classList.toggle("active");
     document.querySelector(".mobile-menu").classList.toggle("active");
